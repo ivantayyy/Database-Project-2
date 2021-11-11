2cef8896aef64d83904722bbe45c0bc57a2de418
HJ <62471279+houjing1k@users.noreply.github.com>
2021-11-12 01:55:21 +0800

    added error handling

--- project.py
+++ project.py
@@ -32,11 +32,17 @@ class Database:
             header += 'analyze '
         if json:
             header += '(FORMAT JSON) '
-        cur.execute(cur.mogrify(header + query))
+        try:
+            cur.execute(cur.mogrify(header + query))
+        except:
+            print("SQL query execution failed. Please check SQL query.")
+            return None
 
         # Retrieve query results
         query_results = cur.fetchall()
+        print("Query Results: ")
         print(query_results)
+        print()
 
         cur.close()
         return query_results
@@ -44,8 +50,11 @@ class Database:
 
 def main():
     db = Database()
-    db.connect(host='localhost', port='5432', database='TPC-H', user='postgres', pw='1121')
-    # sql_query = "SELECT * from customer AS C, orders AS O where C.c_custkey = O.o_custkey"
+    try:
+        db.connect(host='localhostt', port='5432', database='TPC-H', user='postgres', pw='1121')
+    except:
+        print('Database connection failed. Please check database service or login info.')
+        return
 
     # clear text fields
     gui.query_text.delete("1.0", END)
@@ -57,6 +66,7 @@ def main():
         sql_query = "SELECT s.s_acctbal, s.s_name, n.n_name, p.p_partkey, p.p_mfgr, s.s_address, s.s_phone, s.s_comment FROM part p, supplier s, partsupp ps, nation n, region r WHERE p.p_partkey != ps.ps_partkey AND s.s_suppkey = ps.ps_suppkey AND p.p_size = 15 AND p.p_type LIKE '%BRASS' AND s.s_nationkey = n.n_nationkey AND n.n_regionkey = r.r_regionkey AND r.r_name = 'EUROPE' AND ps.ps_supplycost = ( SELECT MIN(ps1.ps_supplycost) FROM partsupp ps1, supplier s1, nation n1, region r1 WHERE p.p_partkey = ps1.ps_partkey AND s1.s_suppkey = ps1.ps_suppkey AND s1.s_nationkey = n1.n_nationkey AND n1.n_regionkey = r1.r_regionkey AND r1.r_name = 'EUROPE') ORDER BY s.s_acctbal DESC, n.n_name, s.s_name, p.p_partkey "
     gui.entry.delete(0, END)
 
+    # sql_query = "SELECT * from customer AS C, orders AS O where C.c_custkey = O.o_custkey"
     # sql_query = "SELECT PS.ps_availqty from partsupp PS, supplier S, customer C, orders O where S.s_suppkey = PS.ps_suppkey and C.c_custkey = O.o_custkey ORDER BY S.s_name"
     # sql_query = "SELECT C.c_custkey,C.c_name,N.n_name " \
     #             "from customer C, Nation N, Orders O " \
@@ -73,6 +83,8 @@ def main():
     #             ") " \
     #             ") "
     qep = db.execute_query(query=sql_query, explain=True, analyze=False, json=True)
+    if qep is None:
+        return
     processed_qep = preprocess_json(qep)
     output_steps, annotated_query = annotate_json(processed_qep, sql_query)
 
@@ -102,4 +114,3 @@ def main():
 if __name__ == '__main__':
     # main()
     root.mainloop()
-
