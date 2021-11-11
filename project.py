f24d7237bf96f0cc8fd74003e647281c427cf9d5
HJ <62471279+houjing1k@users.noreply.github.com>
2021-11-12 02:11:55 +0800

    fix minor annotation bug

--- project.py
+++ project.py
@@ -62,6 +62,8 @@ def main():
     gui.annotation_text.delete("1.0", END)
 
     sql_query = gui.entry.get()
+    if sql_query == "1":
+        sql_query = "SELECT * from customer AS C, orders AS O where C.c_custkey = O.o_custkey"
     if sql_query == "test":
         sql_query = "SELECT s.s_acctbal, s.s_name, n.n_name, p.p_partkey, p.p_mfgr, s.s_address, s.s_phone, s.s_comment FROM part p, supplier s, partsupp ps, nation n, region r WHERE p.p_partkey != ps.ps_partkey AND s.s_suppkey = ps.ps_suppkey AND p.p_size = 15 AND p.p_type LIKE '%BRASS' AND s.s_nationkey = n.n_nationkey AND n.n_regionkey = r.r_regionkey AND r.r_name = 'EUROPE' AND ps.ps_supplycost = ( SELECT MIN(ps1.ps_supplycost) FROM partsupp ps1, supplier s1, nation n1, region r1 WHERE p.p_partkey = ps1.ps_partkey AND s1.s_suppkey = ps1.ps_suppkey AND s1.s_nationkey = n1.n_nationkey AND n1.n_regionkey = r1.r_regionkey AND r1.r_name = 'EUROPE') ORDER BY s.s_acctbal DESC, n.n_name, s.s_name, p.p_partkey "
     gui.entry.delete(0, END)
